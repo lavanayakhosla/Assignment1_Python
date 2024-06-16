1b3780683c911b0bf71a458276cdfe54f4a66b77
lavanayakhosla <144594627+lavanayakhosla@users.noreply.github.com>
2024-06-16 21:48:11 +0530

    Create q10_uppercase.py

---
+++ q10_uppercase.py
@@ -0,0 +1,11 @@
+def convert_to_uppercase(input_string):
+    return input_string.upper()
+
+
+input_string = "Hello, world!"
+
+uppercase_string = convert_to_uppercase(input_string)
+
+
+print("Original string:", input_string)
+print("Uppercase string:", uppercase_string)
